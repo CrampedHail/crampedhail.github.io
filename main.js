91f4d87c201b966a774e1a19ef04a0c8e2eeb4b6
CrampedHail <pawel.a.brandt@gmail.com>
2023-01-27 17:03:27 +0100

    Added tennis to interests and mini-games descriptions

--- main.js
+++ main.js
@@ -212,6 +212,25 @@ function toggleShowDropdown() {
 
 
 
+
+  // Accordion
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
   // Connection Status 
   window.addEventListener('online', ConnectionStatusCheck)
   window.addEventListener('offline', ConnectionStatusCheck)
